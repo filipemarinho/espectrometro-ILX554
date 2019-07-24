12254d3ab172801135ad1205f8bb9860f561d7b9
filipemarinho <filipemarinho@usp.br>
2019-07-24 11:02:47 -0300

    Improve Serial control

--- Software/control_and_read/control_and_read.ino
+++ Software/control_and_read/control_and_read.ino
@@ -1,9 +1,8 @@
-unsigned int CLK = 5; // verde ccd 
-unsigned int ROG = 4;  //azul ccd
-unsigned int t = 30; //meio periodo de clock //T=2*t +5 us
-//unsigned long time1,time2;
+unsigned int CLK = 5;     //Verde 
+unsigned int ROG = 4;     //Azul
+unsigned int t = 30; //meio periodo de clock tint = (2*t +5) us
+unsigned int integra = 1000;
 int a[2089];
-//unsigned int leitura=0;
 unsigned int tint,time1,time2;
 TaskHandle_t Task1;
 TaskHandle_t Task2;
@@ -15,90 +14,98 @@ void setup() {
   pinMode(ROG,OUTPUT);
   pinMode(CLK,OUTPUT);
 
-
-  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
-  xTaskCreatePinnedToCore(
-                    Task1code,   /* Task function. */
-                    "Task1",     /* name of task. */
-                    10000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    &Task1,      /* Task handle to keep track of created task */
-                    0);          /* pin task to core 0 */                  
+  //cria a task que será executada na fução Task1code()
+  xTaskCreatePinnedToCore(Task1code, "Task1", 10000     , NULL, 1         , &Task1, 0   );
+//xTaskCreatePinnedToCore(funcao   ,  nome  , Stack size,     , prioridade, handle, core);                  
   delay(500); 
 
-  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
-  xTaskCreatePinnedToCore(
-                    Task2code,   /* Task function. */
-                    "Task2",     /* name of task. */
-                    10000,       /* Stack size of task */
-                    NULL,        /* parameter of the task */
-                    1,           /* priority of the task */
-                    &Task2,      /* Task handle to keep track of created task */
-                    1);          /* pin task to core 1 */
-    delay(500); 
+  //cria a task que será executada na fução Task2code()
+  xTaskCreatePinnedToCore(Task2code, "Task2", 10000     , NULL, 1         , &Task2, 1   );
+//xTaskCreatePinnedToCore(funcao   ,  nome  , Stack size,     , prioridade, handle, core);
+  delay(500); 
 }
 
 //Controle de Pulsos e tempo de integração
 void Task1code( void * pvParameters ){
-  //Serial.print("Task1 running on core ");
-  //Serial.println(xPortGetCoreID());
+  //Serial.println(xPortGetCoreID());  //retorna o core atual
 
-  for(;;){
-time1 = micros();
-  delayMicroseconds(18*t); //t9 rog pulse low
+ for(;;){
+  time1 = micros();
+  delayMicroseconds(18*t);          //t9 rog pulse low
   digitalWrite(ROG, HIGH);
-  delayMicroseconds(12*t); //t11
-//  leitura = 1;
-  for(int i=0; i<2088; i++)
-    {
-        digitalWrite(CLK, HIGH);
-        delayMicroseconds(t);
-        digitalWrite(CLK, LOW);
-        a[i]=analogRead(A0);
-        delayMicroseconds(t-11);
-    }
-    
-  //leitura=0;
-  delay(1000);
+  delayMicroseconds(12*t);          //t11
+  
+  for(int i=0; i<2088; i++) {
+    digitalWrite(CLK, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(CLK, LOW);
+    a[i]=analogRead(A0);
+    delayMicroseconds(t-11);        // 11uS = adc read time
+  }
+
+  delay(integra + 10);
   digitalWrite(CLK, HIGH);     
   delayMicroseconds(20*t); //t7
   digitalWrite(ROG, LOW);
-  //delay(200);
   time2 = micros();
-  tint = time2-time1; 
-  //Serial.println(time2 - time1); 
-  } 
+  tint = time2-time1;
+ } 
 }
 
-//Leitura do sensor
+//No segundo core:
 void Task2code( void * pvParameters ){
-  //Serial.print("Task2 running on core ");
-  //Serial.println(xPortGetCoreID());
+  //Serial.println(xPortGetCoreID());  //retorna o core atual
 
   for(;;){
     if(Serial.available()){
-      char n;
+      String n;
       n=Serial.read();
-      if (n=='a'){
         
-        for(int x=1; x<=2086;x++){
-              //a[i]=analogRead(A0); 
-              //delayMicroseconds(2*t -1);
-              delayMicroseconds(3); 
-              Serial.print(a[x]);
-              Serial.print(" ");
-              }   
-        Serial.println();
-
+      if (n=='m'){
+        leitura();
       }
-         if (n=='t') {
-        Serial.println(tint);
+      
+      if (n==10m){
+        for (int j=0;j<10;j++) {
+          leitura();
+          delay(tint);
         }
+      }
+      if (n=='t') {
+        Serial.println(tint/1000);
+      }
+      
+      if (n=='w') {
+        integra += 500;
+      }
+      
+      if (n=='s') {
+        integra -= 500;
+      }
+      
+      if (n=='d') {
+        integra += 100;
+      }
+      
+      if (n=='a') {
+        integra -= 100;
+      }
+      
       n=0;
-    }
+   }
   }
 }
 
+//função auxiliar para leitura
+void leitura() {
+  for(int x=1; x<=2086;x++){
+    delayMicroseconds(3); 
+    Serial.print(a[x]);
+    Serial.print(" ");
+  }   
+  Serial.println(); 
+}
+
 void loop() {
+  delay(100);
 }
